7e4f0dcba1036819273e7c931a75c1cd7359415b
TienPD3 <42906458+TienPD3@users.noreply.github.com>
2019-09-02 09:38:50 +0700

    2019/09/02 TienPD3: Add check connection

--- update.bat
+++ update.bat
@@ -3,6 +3,14 @@
 cls
 echo https://tortoisesvn.net/downloads.html
 echo https://github.com/TienPD3/ChromeExtension.git
+
+echo -- Check Connection --
+:ping
+ping 1.2.3.4 -n 1 -w 1000 > nul
+set target=www.google.com
+ping %target% -n 1 | find "TTL="
+if errorlevel==1 goto ping
+
 echo == Initiating system instance variables...
 
 echo. -- Setting the variables...
